2699dd90b7973280d12923fb709b86af28bc619e
Danniela Zárate <lauzarate1227@gmail.com>
2024-11-19 22:01:19 -0500

    Ajustes en imagenes

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from flask import Flask, jsonify, render_template_string
 app = Flask(__name__)
 
 pokeneas = [
-    {"id": 1, "nombre": "Pokenea1", "altura": 1.5, "habilidad": "Fuerza", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p9.png", "frase": "Siempre adelante."},
-    {"id": 2, "nombre": "Pokenea2", "altura": 1.3, "habilidad": "Velocidad", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p8.webp", "frase": "La calma precede a la tormenta."},
-    {"id": 3, "nombre": "Pokenea3", "altura": 1.5, "habilidad": "Fuego", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p7.jpg", "frase": "Siempre adelante."},
-    {"id": 4, "nombre": "Pokenea4", "altura": 1.3, "habilidad": "Agua", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p6.webp", "frase": "La calma precede a la tormenta."},
-    {"id": 5, "nombre": "Pokenea5", "altura": 1.3, "habilidad": "Tierra", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p4.png", "frase": "La calma precede a la tormenta."},
-    {"id": 6, "nombre": "Pokenea6", "altura": 1.3, "habilidad": "Aire", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p1.jpg", "frase": "La calma precede a la tormenta."},
-    {"id": 7, "nombre": "Pokenea7", "altura": 1.3, "habilidad": "Hielo", "imagen": "https://storage.googleapis.com/pokeneas-imagenes/p10.jpg", "frase": "La calma precede a la tormenta."},
+    {"id": 1, "nombre": "Pokenea1", "altura": 1.5, "habilidad": "Fuerza", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke1.png", "frase": "¿Por que tan debil, mano? La fuerza esta en el amor."},
+    {"id": 2, "nombre": "Pokenea2", "altura": 1.3, "habilidad": "Velocidad", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke2.jpeg", "frase": "3 steps ahead, always 3 steps ahead."},
+    {"id": 3, "nombre": "Pokenea3", "altura": 1.5, "habilidad": "Fuego", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke3.jpeg", "frase": "Que tu fuego queme a los demas de amor."},
+    {"id": 4, "nombre": "Pokenea4", "altura": 1.3, "habilidad": "Agua", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke5.jpeg", "frase": "La calma precede a la tormenta."},
+    {"id": 5, "nombre": "Pokenea5", "altura": 1.3, "habilidad": "Tierra", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke6.jpeg", "frase": "Se tan firme como una montaña y nadie te podra mover."},
+    {"id": 6, "nombre": "Pokenea6", "altura": 1.3, "habilidad": "Aire", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke7.jpeg", "frase": "Que tus buenas acciones sean llevadas con el viento, no las muestres.."},
+    {"id": 7, "nombre": "Pokenea7", "altura": 1.3, "habilidad": "Hielo", "imagen": "https://storage.googleapis.com/pokeneas-danniela/poke8.jpeg", "frase": "La sobriedad es la cuspide del ser humano."},
 ]
 
 @app.route('/pokejson', methods=['GET'])
